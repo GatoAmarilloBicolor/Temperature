41b6d80b46b1e0a11d286758d4bc76295ba4fa3e
Yellow Skar <cosmogatokat@gmail.com>
2023-01-25 00:19:44 +0000

    Create App.cpp
    app.cpp

---
+++ App.cpp
@@ -0,0 +1,20 @@
+#include "App.h"
+#include "MainWindow.h"
+
+const char *kTemperaturePath = "/dev/power/acpi_thermal/0";
+
+App::App(void)
+	:	BApplication("application/x-vnd.dw-TestApp")
+{
+	MainWindow *mainwin = new MainWindow();
+	mainwin->Show();
+}
+
+
+int main(int argc, char **argv)
+{
+	App *app = new App();
+	app->Run();
+	delete app;
+	return 0;
+}
